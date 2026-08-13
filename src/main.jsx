790174c1e55078e52dc5360020687f6ab5a15431
jairoow <jairo.oh288@gmail.com>
2026-08-13 14:32:17 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Bitacora from "./bitacora";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Bitacora />
-  </React.StrictMode>
+  <h1 style={{ padding: "40px", fontFamily: "Arial" }}>
+    FUNCIONA
+  </h1>
 );
